ecd06d62c10d0dde00894dea2fdb2250ba57f59c
Varsha Kandi <kandivarsha09@gmail.com>
2024-09-23 15:24:59 -0400

    Made Profile component private by adding it to the PrivateRoute

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+    const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,15 +17,19 @@ const Header = () => {
                 <input type="text" placeholder="search.." className="bg-transparent focus:outline-none text-slate-600 w-24 sm:w-64"></input>
                 <FaSearch className="text-slate-600"/>
             </form>
-            <ul className="flex gap-3">
+            <ul className="flex gap-3 items-center">
                 <Link to="/">
                     <li className="hidden sm:inline hover:underline text-slate-700">Home</li>
                 </Link>
                 <Link to="/about">
                     <li className="hidden sm:inline hover:underline text-slate-700">About</li>
                 </Link>
-                <Link to="/signin">
-                    <li className="hover:underline text-slate-700">Sign In</li>
+                <Link to="/profile">
+                    { currentUser ? (
+                        <img src={currentUser.avatar} alt="avatar" className="rounded-full h-9 w-9 object-cover border-2 border-slate-500"/>
+                    ) : (
+                        <li className="hover:underline text-slate-700">Sign In</li>
+                    )}
                 </Link>
             </ul>
         </div>
