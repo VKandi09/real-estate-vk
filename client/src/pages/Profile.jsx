bdd6543c066558f40b71c6981b1a432e2f23faea
Varsha Kandi <kandivarsha09@gmail.com>
2024-09-23 22:59:32 -0400

    Updated image upload on Profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,19 +1,91 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [filePercentage, setFilePercentage] = useState(0);
+  const [formData, setFormData] = useState({});
 
-  const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+
+    const fileName = new Date().getTime() + file.name;
+
+    const storageRef = ref(storage, fileName);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePercentage(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => 
+          setFormData({...formData, avatar: downloadURL}));
+      }
+    );
+  }
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-4">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt="avatar" className="rounded-full h-24 w-24 object-cover self-center cursor-pointer"/>
-        <input type="text" placeholder="username" defaultValue={currentUser.username} id="username" className="border p-3 rounded-lg"/>
-        <input type="text" placeholder="email" defaultValue={currentUser.email} id="email" className="border p-3 rounded-lg"/>
-        <input type="text" placeholder="password" defaultValue={currentUser.password} id="password" className="border p-3 rounded-lg"/>
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" accept="image/*" ref={fileRef} hidden />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt="avatar"
+          className="rounded-full h-24 w-24 object-cover self-center cursor-pointer"
+        />
+        <p className="text-sm self-center">
+          { fileUploadError ? (
+            <span className="text-red-700">Error uploading image (image must be less than 2 MB)</span>
+          ) : filePercentage > 0 && filePercentage < 100 ? (
+            <span>{`Uploading ${filePercentage}% `}</span>
+          ) : filePercentage === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : ''
+          }
+        </p>
+        <input
+          type="text"
+          placeholder="username"
+          defaultValue={currentUser.username}
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          defaultValue={currentUser.email}
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          defaultValue={currentUser.password}
+          id="password"
+          className="border p-3 rounded-lg"
+        />
         <button className="bg-slate-700 text-white uppercase p-3 rounded-lg hover:opacity-95">
-          Submit
+          Update
         </button>
       </form>
       <div className="flex justify-between mt-4">
@@ -21,7 +93,7 @@ const Profile = () => {
         <span className="text-red-700 cursor-pointer">Sign Out?</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
