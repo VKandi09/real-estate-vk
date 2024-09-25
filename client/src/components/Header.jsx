38924f11e71260322b88cc42243fe149aa58723c
Varsha Kandi <kandivarsha09@gmail.com>
2024-09-24 22:55:38 -0400

    App title, background styling updates. Added create listing UI page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,11 +10,12 @@ const Header = () => {
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
             <Link to="/">
                 <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-                    <span className="text-slate-600">HouseHunt</span>
+                    <span className="text-slate-500">House</span>
+                    <span className="text-slate-700">Hunt</span>
                 </h1>
             </Link>
             <form className="bg-slate-100 rounded-lg p-3 flex items-center">
-                <input type="text" placeholder="search.." className="bg-transparent focus:outline-none text-slate-600 w-24 sm:w-64"></input>
+                <input type="text" placeholder="Search.." className="bg-transparent focus:outline-none text-slate-600 w-24 sm:w-64"></input>
                 <FaSearch className="text-slate-600"/>
             </form>
             <ul className="flex gap-3 items-center">
