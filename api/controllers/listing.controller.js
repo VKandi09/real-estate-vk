c7cc763a4e240aba6faf7582f7c6eaacb6bb8e3d
Varsha Kandi <kandivarsha09@gmail.com>
2024-09-26 12:47:22 -0400

    Added update listing UI

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -16,7 +16,7 @@ export const deleteListing = async(req, res, next) => {
     if(!listing) return next(errorHandler(404, "Listing not found"));
 
     if(req.user.id !== listing.userRef) {
-        return next(errorHandler(404, "You can only delete your own listings"));
+        return next(errorHandler(403, "You can only delete your own listings"));
     }
 
     try {
@@ -33,7 +33,7 @@ export const updateListing = async (req, res, next) => {
     if(!listing) return next(errorHandler(404, "Listing not found"));
 
     if(req.user.id !== listing.userRef){
-        return next(errorHandler(404, "You can only update your own listings"));
+        return next(errorHandler(403, "You can only update your own listings"));
     }
 
     try {
@@ -43,6 +43,18 @@ export const updateListing = async (req, res, next) => {
             {new : true}
         );
         res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getListing = async(req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+
+        if(!listing) return next(errorHandler(404, "Listing not found"));
+
+        res.status(200).json(listing);
     } catch (error) {
         next(error);
     }
